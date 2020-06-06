c21fec35116a90c73e947692a68357876e55061a
uhmseohun <uhmtotow@gmail.com>
2020-06-07 08:57:24 +0900

    t

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import GAuth from 'vue-google-oauth2'
 
 Vue.use(GAuth, {
   clientId: '824660339822-eobgr5cp36cu2hbs38s2u5t7ti1hsbq1.apps.googleusercontent.com',
-  scope: 'email profile'
+  scope: 'email profile youtube.readonly'
 })
 
 Vue.config.productionTip = false
