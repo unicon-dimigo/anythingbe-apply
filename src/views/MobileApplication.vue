6500e82e10448f891a3b74c504737f444598282e
Seohun Uhm <uhmtotow@gmail.com>
2021-03-09 07:22:16 +0900

    F

--- src/views/MobileApplication.vue
+++ src/views/MobileApplication.vue
@@ -2,7 +2,6 @@
 import axios from 'axios'
 import * as firebase from 'firebase'
 import MTopButton from '../components/MTopButton.vue'
-import YoutubeProfile from '../components/YoutubeProfile.vue'
 const app = firebase.initializeApp({
   apiKey: 'AIzaSyAEHtOGwMS8TW-J66WcTGxEdXYs9tLQTBc',
   authDomain: 'anythingbe-b7762.firebaseapp.com',
@@ -17,7 +16,7 @@ const database = app.database()
 
 export default {
   name: 'app',
-  components: { MTopButton, YoutubeProfile },
+  components: { MTopButton },
   data () {
     return {
       agree: false,
@@ -42,6 +41,7 @@ export default {
           etc: null
         },
         platform: {
+          youtube: '',
           twitch: false,
           africa: false,
           facebook: false,
@@ -262,26 +262,6 @@ export default {
         </div>
 
         <div class="app__field">
-          <span class="app__field__label">유튜브 운영 채널</span>
-          <div v-if="form.youtubeId === ''">
-            <button
-              class="google__button"
-              @click="signInGoogle"
-            >
-              <img class="google__logo" src="../assets/google.png">
-              Sign in with Google
-            </button>
-            <span class="app__field__helper">
-              * 구글 계정을 통하여 채널 확인이 이루어집니다.<br>
-              해당 정보는 본인 채널 여부 및 기타 지표 확인을 위하여 사용되며, 자동 가입 등의 절차로 연결되지 않습니다.
-            </span>
-          </div>
-          <span v-else>
-            <youtube-profile :profile="channel" />
-          </span>
-        </div>
-
-        <div class="app__field">
           <span class="app__field__label">채널 카테고리</span>
           <div class="platform__list">
             <div style="display: flex;">
@@ -404,17 +384,17 @@ export default {
         <div class="app__field" style="margin-bottom: 10px;">
           <span class="app__field__label">개인정보 수집 및 이용약관 동의</span>
           <div class="app__field__privacy">
-            anythingBE는 크리에이터 지원시 아래와 같이 개인정보를 수집 및 이용하고 있습니다. 내용을 자세히 읽으신 후 동의 여부를 결정하여 주십시오. 이용자는 개인정보 수집 및 이용에 대한 동의를 거부할 권리가 있습니다. 다만 동의를 거부하는 경우 크리에이터 지원 신청 서비스가 제한될 수 있습니다.
+            ANYTHING BE는 크리에이터 지원시 아래와 같이 개인정보를 수집 및 이용하고 있습니다. 내용을 자세히 읽으신 후 동의 여부를 결정하여 주십시오. 이용자는 개인정보 수집 및 이용에 대한 동의를 거부할 권리가 있습니다. 다만 동의를 거부하는 경우 크리에이터 지원 신청 서비스가 제한될 수 있습니다.
             <br><br>
             1. 수집하는 개인정보<br>
             · 필수항목 : 이름, 성별, 생년월일, 전화번호(휴대전화번호), 이메일 주소, 유튜브 채널 주소<br>
             · 선택항목 : 유튜브 외 추가 운영 중인 플랫폼 관련 정보
             <br><br>
             2. 개인정보의 수집 및 이용 목적<br>
-            anythingBE는 크리에이터로 지원하는 지원자의 지원 의사를 확인하고 본인 확인을 하기 위해 개인정보를 수집 및 이용합니다. 크리에이터 중복 지원을 방지하고 서비스를 부정 이용하는 것을 방지하기 위해 개인정보를 수집 및 이용하며, 크리에이터 지원 심사 및 지원 심사 결과 안내, 기타 각종 안내사항이나 고지사항을 전달하기 위해 개인정보를 수집 및 이용합니다. 또한 추후 발생할 수 있는 분쟁 등을 위하여 개인정보를 보유합니다.
+            ANYTHING BE는 크리에이터로 지원하는 지원자의 지원 의사를 확인하고 본인 확인을 하기 위해 개인정보를 수집 및 이용합니다. 크리에이터 중복 지원을 방지하고 서비스를 부정 이용하는 것을 방지하기 위해 개인정보를 수집 및 이용하며, 크리에이터 지원 심사 및 지원 심사 결과 안내, 기타 각종 안내사항이나 고지사항을 전달하기 위해 개인정보를 수집 및 이용합니다. 또한 추후 발생할 수 있는 분쟁 등을 위하여 개인정보를 보유합니다.
             <br><br>
             3. 개인정보 보유기간<br>
-            anythingBE는 지원자가 개인정보 수집 및 이용에 동의한 날로부터 1년간 개인정보를 보유합니다. 위 1년의 기간은 크리에이터 지원자의 지원 의사 확인 및 심사, 모니터링을 위한 기간입니다. 또한 회사는 개인정보보호법령 등에 따라 특별히 보유하여야 할 필요가 있는 경우에는 법령상 기간을 준수하여 보유하고 있습니다.
+            ANYTHING BE는 지원자가 개인정보 수집 및 이용에 동의한 날로부터 1년간 개인정보를 보유합니다. 위 1년의 기간은 크리에이터 지원자의 지원 의사 확인 및 심사, 모니터링을 위한 기간입니다. 또한 회사는 개인정보보호법령 등에 따라 특별히 보유하여야 할 필요가 있는 경우에는 법령상 기간을 준수하여 보유하고 있습니다.
             <br><br>
             4. 개인정보의 파기<br>
             회사는 지원자가 개인정보 수집 및 이용에 동의한 날로부터 1년간 개인정보를 보유합니다. 위 1년의 기간은 크리에이터 지원자의 지원 의사 확인 및 심사, 모니터링을 위한 기간입니다. 또한 회사는 개인정보보호법령 등에 따라 특별히 보유하여야 할 필요가 있는 경우에는 법령상 기간을 준수하여 보유하고 있습니다.
